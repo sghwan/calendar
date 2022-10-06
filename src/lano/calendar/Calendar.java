521e9da407ab1cad29c3f6bbe6fe8259f43335ee
sghwan <anan123456@naver.com>
2022-10-06 15:54:44 +0900

    feat: 윤년을 판별하는 메서드 추가

--- src/lano/calendar/Calendar.java
+++ src/lano/calendar/Calendar.java
@@ -1,19 +1,31 @@
 package lano.calendar;
 
 public class Calendar {
-	private static final int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDayofMonth(int month) {
-		return maxDays[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+
+		return false;
+	}
+
+	public int getMaxDayofMonth(int year, int month) {
+		if (isLeapYear(year))
+			return LEAP_MAX_DAYS[month - 1];
+
+		return MAX_DAYS[month - 1];
 	}
 
 	public void printCalendar(int year, int month) {
 		System.out.printf("   <<%d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("--------------------");
-		for(int i = 1; i <= getMaxDayofMonth(month); i++) {
+		for (int i = 1; i <= getMaxDayofMonth(year, month); i++) {
 			System.out.printf("%3d", i);
-			if(i % 7 == 0) System.out.println();
+			if (i % 7 == 0)
+				System.out.println();
 		}
 		System.out.println();
 	}
--- src/lano/calendar/Prompt.java
+++ src/lano/calendar/Prompt.java
@@ -5,25 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Prompt {
-	
+
 	private static final String PROMPT = "cal> ";
-	
+
 	public void runPrompt() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Calendar calendar = new Calendar();
 
 		while (true) {
-			System.out.println("월 을 입력하세요.");
+			System.out.println("년도를 입력하세요.");
+			System.out.print(PROMPT);
+			int year = Integer.parseInt(br.readLine());
+
+			System.out.println("월을 입력하세요.");
 			System.out.print(PROMPT);
 			int month = Integer.parseInt(br.readLine());
-			if (month == -1) break;
-			if (month > 12) continue;
-			calendar.printCalendar(2022, month);
+
+			if (month == -1)
+				break;
+			if (month > 12)
+				continue;
+
+			calendar.printCalendar(year, month);
 		}
-		
+
 		System.out.println("END");
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		Prompt p = new Prompt();
 		p.runPrompt();
