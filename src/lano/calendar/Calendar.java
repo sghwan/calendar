ce38b654038b2c67f88cd8157eae04d03fe98709
sghwan <anan123456@naver.com>
2022-10-07 16:49:30 +0900

    refactor: 년, 월을 입력하여 실제 달력을 출력하도록 리팩토링

--- src/lano/calendar/Calendar.java
+++ src/lano/calendar/Calendar.java
@@ -3,7 +3,7 @@ package lano.calendar;
 public class Calendar {
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+	
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 			return true;
@@ -17,14 +17,29 @@ public class Calendar {
 
 		return MAX_DAYS[month - 1];
 	}
+	
+	public int getWeekday(int year, int month) {
+		int lastYear = year - 1;
+		int day = lastYear * 365 + ((lastYear / 4) - (lastYear / 100) + (lastYear / 400));
+		boolean flag = isLeapYear(year);
+	 
+		for (int i = 0; i < month - 1; i++) {
+			if(flag) day += LEAP_MAX_DAYS[i];
+			else day += MAX_DAYS[i];
+		}
+		
+		return day % 7 + 1;
+	}
 
-	public void printCalendar(int year, int month, int weekday) {
+	public void printCalendar(int year, int month) {
+		int weekday = getWeekday(year, month);
+		
 		System.out.printf("   <<%d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
 		
-		for (int j = 0; j < weekday; j++) {
-			System.out.print("   ");
+		for (int i = 0; i < weekday; i++) {
+			System.out.printf("   ");
 		}
 		
 		for (int i = 1; i <= getMaxDayOfMonth(year, month); i++) {
@@ -32,7 +47,7 @@ public class Calendar {
 			if ((i + weekday) % 7 == 0)
 				System.out.println();
 		}
-		
+
 		System.out.println();
 		System.out.println();
 	}
--- src/lano/calendar/Prompt.java
+++ src/lano/calendar/Prompt.java
@@ -5,27 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Prompt {
-	
-	public int parseDay(String weekday) {
-		switch (weekday) {
-		case "SU":
-			return 0;
-		case "MO":
-			return 1;
-		case "TU":
-			return 2;
-		case "WE":
-			return 3;
-		case "TH":
-			return 4;
-		case "FR":
-			return 5;
-		case "SA":
-			return 6;
-		default:
-			return -1;
-		}
-	}
 
 	public void runPrompt() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,26 +14,19 @@ public class Prompt {
 			System.out.println("년도를 입력하세요. (-1: 프로그램 종료)");
 			System.out.print("YEAR> ");
 			int year = Integer.parseInt(br.readLine());
-			if(year == - 1) break;
+			if (year == -1)
+				break;
 
 			System.out.println("월을 입력하세요. (1 ~ 12)");
 			System.out.print("MONTH> ");
 			int month = Integer.parseInt(br.readLine());
 			if (month > 12 || month < 1) {
 				System.out.println("잘못 입력 하셨습니다. 월은 1 ~ 12사이 숫자를 입력해 주세요");
-				continue;
-			}
-			
-			System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
-			System.out.print("WEEKDAY> ");
-			String str_weekday = br.readLine();
-			int weekday = parseDay(str_weekday);
-			if (weekday == -1) {
-				System.out.println("잘못 입력 하셨습니다. 요일은 SU, MO, TU, WE, TH, FR, SA 중 하나를 입력해 주세요");
+				System.out.println();
 				continue;
 			}
 
-			calendar.printCalendar(year, month, weekday);
+			calendar.printCalendar(year, month);
 		}
 
 		System.out.println("END");
